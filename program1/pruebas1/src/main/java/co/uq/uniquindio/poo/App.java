d3e4dc1bae95c91954c0831494696f9be9ef81bf
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2023-08-24 21:31:33 -0500

    soluciones de problemas inciales

---
+++ program1/pruebas1/src/main/java/co/uq/uniquindio/poo/App.java
@@ -0,0 +1,13 @@
+package co.uq.uniquindio.poo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
