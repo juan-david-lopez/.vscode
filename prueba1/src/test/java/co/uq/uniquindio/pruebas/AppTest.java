b06c1160bd75189de751442454eec27c13c32a33
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2023-08-31 19:16:10 -0500

    se modifican los imports

--- prueba1/src/test/java/co/uq/uniquindio/pruebas/AppTest.java
+++ prueba1/src/test/java/co/uq/uniquindio/pruebas/AppTest.java
@@ -1,9 +1,9 @@
 
 package co.uq.uniquindio.pruebas;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
