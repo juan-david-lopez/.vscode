43245e0be0d03d1b81f8a2af56277fe567e1b92a
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2023-09-27 09:42:57 -0500

    actualizamos

--- .vscode/torneo1/src/main/java/co/edu/uniquindio/poo/App.java
+++ .vscode/torneo1/src/main/java/co/edu/uniquindio/poo/App.java
@@ -2,19 +2,7 @@ package co.edu.uniquindio.poo;
 
 /**
  * Hello world!
- *  /persona:
--nombre 
--profession
--actividad 
-+seleccionar
-/profesor:
-- trabajo 
-+preparar
-/profesor ingeniero:
-- materia
-+ tema
- * 
- * 
+ *
  */
 public class App {
     public static void main(String[] args) {
