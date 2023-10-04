750b9295f5c1886101bb4ec6c60c7244c542fd89
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2023-10-04 09:44:43 -0500

    correciones y rediseño proyecto

--- .vscode/proyectofinaltorneo/src/main/java/co/edu/uniquindio/poo/Aplication/App.java
+++ .vscode/proyectofinaltorneo/src/main/java/co/edu/uniquindio/poo/Aplication/App.java
@@ -1,4 +1,4 @@
-package co.edu.uniquindio.poo;
+package co.edu.uniquindio.poo.Aplication;
 
 /**
  * Hello world!
---
+++ .vscode/proyectofinaltorneo/src/main/java/co/edu/uniquindio/poo/Interfaces/jueces.java
@@ -0,0 +1,22 @@
+package co.edu.uniquindio.poo.Interfaces;
+
+public class jueces {
+    public jueces(){
+
+    }
+    private String ID;
+    private int numVecesJuez;
+    
+    public String getID() {
+        return ID;
+    }
+    public void setID(String iD) {
+        ID = iD;
+    }
+    public int getNumVecesJuez() {
+        return numVecesJuez;
+    }
+    public void setNumVecesJuez(int numVecesJuez) {
+        this.numVecesJuez = numVecesJuez;
+    }
+}
