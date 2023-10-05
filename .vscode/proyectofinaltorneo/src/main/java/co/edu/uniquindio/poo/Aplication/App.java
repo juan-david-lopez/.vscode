ddf1816f5782ae0a85bfa2ebdf1918e7eae3ad3e
Juan david lopez h <juand.lopezh@uqvirtual.edu.co>
2023-10-04 21:36:02 -0500

    prueba java

--- .vscode/proyectofinaltorneo/src/main/java/co/edu/uniquindio/poo/Aplication/App.java
+++ .vscode/proyectofinaltorneo/src/main/java/co/edu/uniquindio/poo/Aplication/App.java
@@ -1,12 +1,17 @@
 package co.edu.uniquindio.poo.Aplication;
 
 import javax.swing.JFrame;
+import co.edu.uniquindio.poo.interfazUser.pantallaInicio;
 
 /**
  * Hello world!
  *
  */
-public class App {
-  JFrame pantallaInicio= new JFrame();
+public class App{
+
+  pantallaInicio ventana1 = new pantallaInicio();
+  pantallaInicio.setVisible
+  
   
 }
+
--- .vscode/proyectofinaltorneo/src/main/java/co/edu/uniquindio/poo/interfazUser/pantallaInicio.java
+++ .vscode/proyectofinaltorneo/src/main/java/co/edu/uniquindio/poo/interfazUser/pantallaInicio.java
@@ -2,9 +2,13 @@ package co.edu.uniquindio.poo.interfazUser;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class pantallaInicio {
+public class pantallaInicio implements J{
+    JFrame pantall2 = new JFrame("prueba1");
+    pantall2.
+    
     public void crearPantalla(){
         JPanel pantalla1= new JPanel(new BoxLayout(null, 0), false);
         JButton boton1= new JButton();
